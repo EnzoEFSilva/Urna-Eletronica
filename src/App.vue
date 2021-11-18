1eec223fee037f9d0a31f1675d0786003f34b075
enzo@Furtuoso <enzo.furtoso@youx.group.com.br>
2021-11-18 08:56:30 -0300

    base

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+
+    <div class="urna"></div>
+    <Teclado />
+   
+  </div>
+</template>
+
+<script>
+import './css/global.css'
+import Teclado from './components/Teclado.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Teclado
+  }
+}
+</script>
+
+<style>
+#app {
+background-color: var(--background-color);
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+
+.urna {
+  width: 1000px;
+  height: 500px;
+  background-color: var(--ballot-box-background-color);
+  padding: 30px;
+  border-radius: 7px;
+  display: flex;
+}
+</style>
---
+++ src/components/Teclado.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="urna-teclado">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Teclado'
+}
+</script>
+
+<style>
+
+</style>
